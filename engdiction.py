b66562acd0d2d0eb0b9d086a25c037828f473aae
Precious <ibudeprecious87@gmail.com>
2025-07-06 14:11:55 +0100

    solved a bug

--- engdiction.py
+++ engdiction.py
@@ -1,16 +1,16 @@
 import json
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QLabel, QPushButton
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPushButton
 
 def finder():
     data = json.load(open('001 data.json', 'r'))
     try:
-        found = data[text.text().strip()]
+        found = data[text.text().strip().lower()]
         final = ''
         for a in found:
             final += a + '\n\n'
         result.setText(f'This is/are the meaning:\n{final}')
     except:
-        result.setText(f'{text.text()} is not an English Word. Please enter an English Word')
+        result.setText(f'Word not found')
 
 app = QApplication([])
 window = QWidget()
@@ -23,12 +23,15 @@ layout = QVBoxLayout()
 description = QLabel('Input the Unknown Word here')
 layout.addWidget(description)
 
+layout2 = QHBoxLayout()
+layout.addLayout(layout2)
+
 text = QLineEdit()
-layout.addWidget(text)
+layout2.addWidget(text)
 
 btn = QPushButton('Search')
 btn.clicked.connect(finder)
-layout.addWidget(btn)
+layout2.addWidget(btn)
 
 result = QLabel('')
 layout.addWidget(result)
